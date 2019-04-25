f3b984cda402b717169a00a017a5657bf9785235
xy1219.zhao <xy1219.zhao@samsung.com>
2019-04-25 06:56:55 +0000

    2.11.0.Beta

--- sed.sh
+++ sed.sh
@@ -1,2 +1,7 @@
-sed -i s/2.7.0.Beta/2.8.0.Beta/ blog-web/pom.xml
 blog-web/src/main/resources/templates/index.ftl  
+
+sed -i s/2.9.0.Beta/2.10.0.Beta/ blog-web/pom.xml
+sed -i s/2.9.0.Beta/2.10.0.Beta/ blog-admin/pom.xml
+sed -i s/2.9.0.Beta/2.10.0.Beta/ blog-core/pom.xml
+sed -i s/2.9.0.Beta/2.10.0.Beta/ blog-spider/pom.xml
+sed -i s/2.9.0.Beta/2.10.0.Beta/ pom.xml
